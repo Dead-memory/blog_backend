8f33ada6e2c948e1338df10393a2eb522245f427
Dofe <thibo1503@gmail.com>
2022-12-12 22:27:39 +0100

    code reorganized

--- migration/src/m20221211_000001_create_user_table.rs
+++ migration/src/m20221211_000001_create_user_table.rs
@@ -7,24 +7,21 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         manager
-            .create_table(
-                Table::create()
-                    .table(User::Table)
-                    .if_not_exists()
-                    .col(ColumnDef::new(User::Id).integer().not_null().auto_increment().primary_key())
-                    .col(ColumnDef::new(User::Pseudo).string().not_null())
-                    .col(ColumnDef::new(User::About).string().not_null())
-                    .col(ColumnDef::new(User::InscriptionDate).date().not_null())
-                    .col(ColumnDef::new(User::InscriptionDate).date().not_null())
-                    .to_owned(),
+            .create_table(Table::create()
+                .table(User::Table)
+                .if_not_exists()
+                .col(ColumnDef::new(User::Id).integer().not_null().auto_increment().primary_key())
+                .col(ColumnDef::new(User::Pseudo).string().not_null())
+                .col(ColumnDef::new(User::About).string().not_null())
+                .col(ColumnDef::new(User::InscriptionDate).date().not_null())
+                .col(ColumnDef::new(User::InscriptionDate).date().not_null())
+                .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .drop_table(Table::drop().table(User::Table).to_owned())
-            .await
+        manager.drop_table(Table::drop().table(User::Table).to_owned()).await
     }
 }
 
--- migration/src/m20221212_000002_create_role_table.rs
+++ migration/src/m20221212_000002_create_role_table.rs
@@ -8,15 +8,14 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .create_table(
-                Table::create()
-                    .table(Role::Table)
-                    .if_not_exists()
-                    .col(ColumnDef::new(Role::Id).integer().not_null().auto_increment().primary_key())
-                    .col(ColumnDef::new(Role::Name).string().not_null())
-                    .col(ColumnDef::new(Role::Description).string().not_null())
-                    .to_owned(),
+        manager.create_table(
+            Table::create()
+                .table(Role::Table)
+                .if_not_exists()
+                .col(ColumnDef::new(Role::Id).integer().not_null().auto_increment().primary_key())
+                .col(ColumnDef::new(Role::Name).string().not_null())
+                .col(ColumnDef::new(Role::Description).string().not_null())
+                .to_owned(),
         ).await?;
 
         manager.create_table(
@@ -31,11 +30,8 @@ impl MigrationTrait for Migration {
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        manager
-            .drop_table(Table::drop().table(RolesUsers::Table).to_owned()).await?;
-
-        manager
-            .drop_table(Table::drop().table(Role::Table).to_owned()).await
+        manager.drop_table(Table::drop().table(RolesUsers::Table).to_owned()).await?;
+        manager.drop_table(Table::drop().table(Role::Table).to_owned()).await
     }
 }
 
