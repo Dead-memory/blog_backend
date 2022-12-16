e76e5773dfe046e0d080d83f4e2a62c8f0a9e9e1
tibo1503 <thibo1503@gmail.com>
2022-12-16 10:59:22 +0100

    Unique article

---
+++ migration/src/m20221216_000011_set_unique_field_into_user_table.rs
@@ -0,0 +1,39 @@
+use sea_orm_migration::prelude::*;
+
+use super::m20221211_000001_create_user_table::Article;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+const PICTURE_URL_FIELD_NAME: &'static str = "picture_url";
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        
+        manager.alter_table(
+            sea_query::Table::alter()
+                .table()
+                .modify_column(ColumnDef::new(User::Pseudo)
+                    .string()
+                    .not_null()
+                    .unique_key()
+                )
+                .to_owned()
+        ).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.alter_table(
+            sea_query::Table::alter()
+                .table(Article::Table)
+                .modify_column(ColumnDef::new(Alias::new("title"))
+                    .string()
+                    .not_null()
+                )
+                .to_owned()
+        ).await
+    }
+}
+
